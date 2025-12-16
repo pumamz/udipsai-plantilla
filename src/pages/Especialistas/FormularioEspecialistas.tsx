a349f1fe2a850d45ce2c1fb4251dc8fe157b6c49
Alexis Ramirez <alexis0991768994@gmail.com>
2025-12-16 11:08:22 -0500

    feat: add specialist management pages for assignments, details, and forms, updating package dependencies.

---
+++ src/pages/Especialistas/FormularioEspecialistas.tsx
@@ -0,0 +1,21 @@
+import ComponentCard from "../../components/common/ComponentCard";
+import PageBreadcrumb from "../../components/common/PageBreadCrumb";
+import PageMeta from "../../components/common/PageMeta";
+import Input from "../../components/form/input/InputField";
+import Label from "../../components/form/Label";
+
+export default function FormularioEspecialistas() {
+    return (
+        <div>
+            <PageMeta 
+                title="Formulario de Especialistas | Udipsai"
+                description="Formulario para la gestión de Especialistas en Udipsai"
+                />
+            <PageBreadcrumb
+                pageTitle="Formulario de Especialistas"
+                
+            />
+           
+        </div>
+    );
+}
